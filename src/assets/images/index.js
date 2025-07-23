ad8f95d4e7250d72cf3803b907f986c03a45d60f
Kadockh <andreykadoch20@gmail.com>
2025-07-23 17:46:58 -0300

    feat(images): add new images and import module

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import cafe from "./cafe.png";
+import sede from "./sede.png";
+import entrevista from "./entrevista.png";
+import luizaeali from "./luizaeali.png";
+import mariana from "./mariana.png";
+
+
+export { cafe, sede, entrevista, luizaeali, mariana};
